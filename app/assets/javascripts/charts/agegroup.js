4d38b8937d63f3732d3fcca0867e4220c6304e5c
Maxim Tocarev <max.tocarev@gmail.com>
2017-12-06 11:27:28 -0800

    Axis working

--- app/assets/javascripts/charts/agegroup.js
+++ app/assets/javascripts/charts/agegroup.js
@@ -4,15 +4,69 @@ import * as scale from "d3-scale";
 window.d3 = d3;
 window.scale = scale;
 
-var width = 250,
+
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    mainWidth = 600 - margin.left - margin.right,
+    mainHeight = 400 - margin.top - margin.bottom;
+
+var width = (mainWidth / 2),
     barHeight = 20;
 
 var x = scale.scaleLinear()
-  .domain([0,12])
   .range([0,width]);
 
-const drawChart = (gender, data) => {
+// var y = scale.scaleOrdinal()
+//   .domain(["85+", "80-84", "75-79", "70-74", "65-69", "60-64", "55-59",
+//            "50-54", "45-49", "40-44", "35-39", "30-34", "25-29", "20-24",
+//            "15-19", "10-14", "5-9", "0-4"])
+//   // .rangeRound([0,360], .1);
+//   // .range([0,360]);
 
+var yBand = scale.scaleBand()
+  .domain(["", "85+", "80-84", "75-79", "70-74", "65-69", "60-64", "55-59",
+           "50-54", "45-49", "40-44", "35-39", "30-34", "25-29", "20-24",
+           "15-19", "10-14", "5-9", "0-4"])
+   .rangeRound([0,380]);
+
+window.yBand = yBand;
+
+var yAxis = d3.axisLeft()
+  .scale(yBand);
+
+const generateXaxis = (sc) => (
+  d3.axisBottom()
+      .scale(sc)
+      .tickValues([0,2,4,6,8,10])
+      .tickFormat(d => d + "%")
+);
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  var mainChart = d3.select('.chart')
+      .attr("width", mainWidth + margin.left + margin.right)
+      .attr("height", mainHeight + margin.top + margin.bottom);
+
+  mainChart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(34,360)")
+      .call(generateXaxis(x.domain([12,0])));
+
+  mainChart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(299,360)")
+      .call(generateXaxis(x.domain([0,12])));
+
+  mainChart.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(34,-10)")
+      .call(yAxis);
+});
+
+
+x.domain([0,12]);
+
+const drawChart = (gender, data) => {
 
   const offset = (d) => {
     if (gender === 'females') {
@@ -40,7 +94,20 @@ const drawChart = (gender, data) => {
       .attr("y", barHeight / 2)
       .attr("dy", ".35em")
       .text((d) => d.pct);
+
+  // if (gender === 'males') {
+  //   chart.append("g")
+  //     .attr("class", "x axis")
+  //     .attr("transform", "translate(34,360)")
+  //     .call(xMAxis);
+  // } else {
+  //   chart.append("g")
+  //     .attr("class", "x axis")
+  //     .attr("transform", "translate(299,360)")
+  //     .call(xAxis);
+  // }
 };
+
 
 
 // const drawChart = (gender, data) => {
--- app/assets/javascripts/entry.js
+++ app/assets/javascripts/entry.js
@@ -4,18 +4,18 @@ import {extractGender} from './util/selectors.js';
 import drawChart from './charts/agegroup';
 
 
-fetchPopulationByCountryYear('AF', 1991)
+fetchPopulationByCountryYear('AF', 1999)
   .then((res) => {
     if ((res) && (res[1][2])) {
-      // console.log(res[1][45]);
-      // console.log(res);
+      console.log(res[1][45]);
+      console.log(res);
 
       let parsed = parseData(res);
       let males = extractGender(parsed, 'M');
       let females = extractGender(parsed, 'F');
-      // console.log(parsed);
-      // console.log(males);
-      // console.log(females);
+      console.log(parsed);
+      console.log(males);
+      console.log(females);
 
       drawChart('males', males);
       drawChart('females', females);
